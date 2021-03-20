22d8e2b5b41c55a5040565d77e9035e7c05eb051
Akram El-Gaouny <aelga098@uottawa.ca>
2021-03-20 09:53:53 -0400

    Added the functioon

--- Value-Investing.py
+++ Value-Investing.py
@@ -2,13 +2,26 @@ import numpy as np #numerical computing library
 import pandas as pd #tabular data
 import xlsxwriter #writting to excel
 import requests #http requests
-from scipy import stats 
+#from scipy import stats 
 import math
 import json
 
-#input from user
-ticker = input("Enter the ticker symbol you would like to analyse: ")
-country = input("Enter the country of the company: ")
+## Global yf api
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-statistics"
+
+
+def getMetrics(ticker, country):
+    querystring = {"symbol":ticker,"region":country}
+
+    headers = {
+    'x-rapidapi-key': "4e08668ab1mshd05172b533fcfb5p1809a2jsnd750a952af5e",
+    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
+    }
+ 
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return response
+
 
 # metrics come from this article 
 # https://www.investopedia.com/articles/fundamental-analysis/09/five-must-have-metrics-value-investors.asp
@@ -29,18 +42,22 @@ recommendationKey = False
     #API code
 catch(not equal 200) """
 
+
+
+
+
+
 #Api Code
 #make this into a method 
-url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-statistics"
 
-querystring = {"symbol":ticker,"region":country}
 
-headers = {
-    'x-rapidapi-key': "4e08668ab1mshd05172b533fcfb5p1809a2jsnd750a952af5e",
-    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
-    }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+
+
+
+
+
+
 
 #printing the data for texts
 #print(response.text)
@@ -49,9 +66,27 @@ response = requests.request("GET", url, headers=headers, params=querystring)
 #NAME OF ERROR JSONDecodeError
 #use this to do the try catch statment to get the calues
 #Like a loop that keeps asking for the two inputs then calls the API function 
-json_object = json.loads(response.text)
+json_object = None
+response = None
 
-#setting values
+isInvalid = True
+while (isInvalid):
+    
+    #input from user
+    ticker = input("Enter the ticker symbol you would like to analyse: ").strip().upper()
+    country = input("Enter the country of the company: ").strip().upper()
+
+    response = getMetrics(ticker, country)
+
+    try:
+        json_object = json.loads(response.text)
+        isInvalid = False
+    except(json.decoder.JSONDecodeError):
+        print("ERROR")
+        isInvalid = True
+
+#print(response)
+
 if int(json_object["defaultKeyStatistics"]["pegRatio"]["raw"]) != None:
     peg_ratio = int(json_object["defaultKeyStatistics"]["pegRatio"]["raw"])
 if int(json_object["defaultKeyStatistics"]["priceToBook"]["raw"]) != None:
@@ -71,6 +106,7 @@ print(pb_ratio)
 print(debtEquity_ratio)
 print(json_object["financialData"]["recommendationKey"])
 print(recommendationKey)
+
 #search for target low and high prices
 #go through financial data and prices to see if there is any more usefull metrics
 #get market cap
