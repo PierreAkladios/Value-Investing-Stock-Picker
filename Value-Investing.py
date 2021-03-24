4225e31f767bb6e82e58c60d8f30d2adb99d9c4b
Pierre Akladios <pierreakladios@gmail.com>
2021-03-23 21:24:46 -0400

    adding helper methods

--- Value-Investing.py
+++ Value-Investing.py
@@ -11,6 +11,7 @@ url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-statistics"
 
 
 def getMetrics(ticker, country):
+    #Api Code
     querystring = {"symbol":ticker,"region":country}
 
     headers = {
@@ -25,40 +26,19 @@ def getMetrics(ticker, country):
 
 # metrics come from this article 
 # https://www.investopedia.com/articles/fundamental-analysis/09/five-must-have-metrics-value-investors.asp
-price = 0
-earning = 0
-book = 0
-debt = 0
-equity = 0 
-debtEquity_ratio = 0 # not too high but not too low
-pe_ratio = 0 # good for compagnies in same industry lower = better
-pb_ratio = 0 #<1 is good
-freeCachFlow = 0 #the higher the better
-peg_ratio = 0 # than 1 is good
-marketCap = 0
-recommendationKey = False
-cachCap_ratio = 0
+price = None
+debtEquity_ratio = None # not too high but not too low
+pb_ratio = None #<1 is good
+freeCachFlow = None #the higher the better
+peg_ratio = None # than 1 is good
+marketCap = None
+recommendationKey = None
+cachCap_ratio = None
+non_null_metrics = None
 
 """ try:
     #API code
 catch(not equal 200) """
-
-
-
-
-
-
-#Api Code
-#make this into a method 
-
-
-
-
-
-
-
-
-
 
 #printing the data for texts
 #print(response.text)
@@ -103,12 +83,42 @@ if json_object["financialData"]["recommendationKey"] != None:
         recommendationKey = True
 
 def cachCap_ratio(freeCachFlow, marketCap):
-    if freeCachFlow != 0 and marketCap != 0:
+    if freeCachFlow != None and marketCap != None:
        return freeCachFlow/marketCap
     return 0
 
-def algo_picker(peg_ratio, pb_ratio, debtEquity_ratio, cachCap_ratio, recommendationKey):
+def null_checker(peg_ratio, pb_ratio, debtEquity_ratio, cachCap_ratio, recommendationKey):
+    if peg_ratio != None:
+        non_null_metrics+=1
+    if pb_ratio != None:
+        non_null_metrics+=1
+    if debtEquity_ratio != None:
+        non_null_metrics+=1
+    if cachCap_ratio != None:
+        non_null_metrics +=1 
+    if recommendationKey != None:
+        non_null_metrics +=1
+
+def peg(peg_ratio):
     pass
+def pb(pb_ratio): 
+    pass
+def debtEquity(debtEquity_ratio):
+    pass
+
+def algo_picker(peg_ratio, pb_ratio, debtEquity_ratio, cachCap_ratio, recommendationKey):
+    total = 0
+    number_of_metrics = null_checker(peg_ratio, pb_ratio, debtEquity_ratio, cachCap_ratio, recommendationKey)
+    #check recommendation key
+    if recommendationKey == True:
+        total +=1
+    #check Freecachflow:
+    if cachCap_ratio != None:
+        total += cachCap_ratio
+    #call helper functions for the other 3 methods
+    if peg_ratio!=0
+    
+    return total/number_of_metrics
 
 print(peg_ratio)
 print(pb_ratio)
